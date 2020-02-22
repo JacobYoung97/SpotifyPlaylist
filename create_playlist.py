c9ed050649530e8da2c3523e857fb90f2c323c24
Jacob Young <jyoung17@uoguelph.ca>
2020-02-21 19:52:35 -0500

    Skeleton class to create later

---
+++ create_playlist.py
@@ -0,0 +1,26 @@
+
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+    
+    # Step 1: Log Into Youtube
+    def get_youtube_client(self):
+        pass
+    
+    # Step 2: Grab Our Liked Videos
+    def get_liked_videos(self):
+        pass
+    
+    # Step 3: Create A New Playlist
+    def create_playlist(self):
+        pass
+    
+    # Step 4: Search For the Song
+    def get_spotify_url(self):
+        pass
+    
+    # Step 5: Add this song into the new Spotify playlist
+    def add_song_to_playlist(self):
+        pass
