c322b39212f1e9829c33e05f60cd8de4aa6729d0
Jacob Young <jyoung17@uoguelph.ca>
2020-04-14 12:08:30 -0400

    Get the YouTube Client

--- create_playlist.py
+++ create_playlist.py
@@ -1,16 +1,42 @@
 import json
 import requests
+import os
+
 from secrets import spotify_user_id, spotify_token
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
 class CreatePlaylist:
 
     def __init__(self):
         self.user_id = spotify_user_id
         self.spotify_token = spotify_token
+        self.youtube_client = self.get_youtube_client()
     
     # Step 1: Log Into Youtube
     def get_youtube_client(self):
-        pass
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = "client_secret.json"
+
+        # Get credentials and create an API client
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, scopes
+            )
+        credentials = flow.run_console()
+
+        # from the Youtube DATA API
+        youtube_client = googleapiclient.discovery.build(
+                api_service_name, api_version, credentials=credentials
+            )
+
+        return youtube_client
     
     # Step 2: Grab Our Liked Videos
     def get_liked_videos(self):
